4dc6de49ba2b0fbed38aa63f2a3b9d5ee19a62e2
Beatrice Tan <113320828+beartisan@users.noreply.github.com>
2023-04-22 17:07:03 -0400

    appointments tables, controllers, model

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -11,23 +11,27 @@ namespace hospital_project.Models
     { 
         [Key]
         public int appointment_id { get; set; }
-
         public string appointment_name { get; set; }
-
         public DateTime appointment_date { get; set; }
-
         //appointment belongs to one patient
         [ForeignKey("Patient")]
-
         public int patient_id { get; set; }
+        public string patient_condition { get; set; }
         public virtual Patient Patient { get; set; }
-
         //appointment belongs to physician
         [ForeignKey("Physician")]
-
         public int physician_id { get; set; }
         public virtual Physician Physician { get; set; }
     }
+    public class AppointmentDto
+    {
+        public int appointment_id { get; set; }
+        public string appointment_name { get; set; }
+        public DateTime appointment_date { get; set; }
+        public int patient_id { get; set; }
+        public string patient_condition { get; set; }
+        public int physician_id { get; set; }
 
-    
+    }
+
 }
