690095f981ebb3e49ebbf7b060a0675af5734ce6
Beatrice Tan <113320828+beartisan@users.noreply.github.com>
2023-04-22 18:01:07 -0400

    patient controller code

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -1,89 +1,160 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using hospital_project.Models;
+using hospital_project.Models.ViewModels;
+using System.Diagnostics;
+
+using Microsoft.AspNet.Identity;
 
 namespace hospital_project.Controllers
 {
     public class AppointmentController : Controller
     {
-        // GET: Appointment
-        public ActionResult Index()
+        private static readonly HttpClientCertificate client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        static AppointmentController()
         {
-            return View();
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44324/api/appointmentdata/");
         }
 
-        // GET: Appointment/Details/5
+        // GET: Appointment/List
+        public ActionResult List()
+        {
+            //objective: communicate to appointments data api to retrieve a list of appointments
+            // curl https://localhost:44324/api/AppointmentData/ListApppointments
+
+            string url = "listappointments";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            IEnumerable<AppointmentDto> Appointments = response.Content.ReadAsAsync<IEnumerable<AppointmentDto>>().Result;
+
+            return View(Appointments);
+        }
+
+        // GET: Appointment/FindAppointment/1
         public ActionResult Details(int id)
         {
-            return View();
+            //objective: look for the appointment through appointments data api
+            // curl https://localhost:44324/api/AppointmentData/FindApppointment/{id}
+
+            string url = "findappointments/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            AppointmentDto SelectedAppointment = response.Content.ReadAsAsync<AppointmentDto>().Result;
+
+            return View(SelectedAppointment);
         }
 
-        // GET: Appointment/Create
-        public ActionResult Create()
+        // GET: Appointment/New
+        public ActionResult New()
         {
             return View();
         }
 
         // POST: Appointment/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Appointment Appointment)
         {
-            try
-            {
-                // TODO: Add insert logic here
+            //objective: create a ne appointment to add in the list of data api
+            // curl -H "Content-Type: application/json" -d @Appointment.json https://localhost:44324/api/AppointmentData/AddApppointment
 
-                return RedirectToAction("Index");
-            }
-            catch
+            string url = "addappointment";
+            string jsonpayload = jss.Serialize(Appointment);
+
+            HttpContent content = new StringContent(jsonpayload);
+
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
             {
-                return View();
+                return RedirectToAction("List");
             }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
         }
 
         // GET: Appointment/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            //objective: look for the appointment through appointments data api
+
+            string url = "findappointment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            AppointmentDto SelectedAppointment = response.Content.ReadAsAsync<AppointmentDto>().Result;
+
+            return View(SelectedAppointment);
         }
 
-        // POST: Appointment/Edit/5
+        // POST: Appointment/Update/1
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Update(int id, Appointment Appointment)
         {
-            try
-            {
-                // TODO: Add update logic here
+            //objective: look for the appointment through appointments data api
 
-                return RedirectToAction("Index");
-            }
-            catch
+            string url = "updateappointment/" + id;
+            string jsonpayload = jss.Serialize(Appointment);
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            HttpContent content = new StringContent(jsonpayload);
+            AppointmentDto SelectedAppointment = response.Content.ReadAsAsync<AppointmentDto>().Result;
+
+            if (response.IsSuccessStatusCode)
             {
-                return View();
+                return RedirectToAction("List");
             }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+
         }
 
-        // GET: Appointment/Delete/5
+        // GET: Appointment/DeleteConfirm/5
+        public ActionResult DeleteConfirm(int id)
+        {
+            //objective: delete the appointment through appointments data api
+
+            string url = "findappointment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            AppointmentDto SelectedAppointment = response.Content.ReadAsAsync<AppointmentDto>().Result;
+
+            return View(SelectedAppointment);
+        }
+
+        // POST: Appointment/Delete/1
+        [HttpPost]
         public ActionResult Delete(int id)
         {
-            return View();
-        }
+            //objective: delete the appointment through appointments data api
 
-        // POST: Appointment/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+            string url = "deleteappointment/" + id;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            
 
-                return RedirectToAction("Index");
-            }
-            catch
+            if (response.IsSuccessStatusCode)
             {
-                return View();
+                return RedirectToAction("List");
             }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
         }
     }
 }
